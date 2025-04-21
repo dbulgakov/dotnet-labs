bee00662ce23cd6d62391ba2b901b988a3d94f94
Dmytro Bulhakov <mail@dbulgakov.com>
2025-04-21 14:07:14 +0300

    Add missing files

---
+++ lab02/lab02/Program.cs
@@ -0,0 +1,20 @@
+﻿using lab02;
+
+ConsoleHelpers.Log("Program Started");
+
+var account = new BankAccount();
+var monitor = new AccountMonitor();
+
+// Subscribe monitor method to both events
+account.BalanceChangedDeposit += AccountMonitor.OnBalanceChanged;
+account.BalanceChangedWithdraw += AccountMonitor.OnBalanceChanged;
+
+account.Deposit(200);
+account.Withdraw(50);
+account.Withdraw(300);
+account.Deposit(150);
+
+ConsoleHelpers.Log($"Final balance: {account.Balance:C}");
+
+ConsoleHelpers.Log("Program Finished. Press any key to exit.");
+Console.ReadKey();
