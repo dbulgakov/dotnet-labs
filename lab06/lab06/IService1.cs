db41ce0bc635be49f2b3dd1613009c7d6abda548
Dmytro Bulhakov <mail@dbulgakov.com>
2025-04-27 12:06:12 +0300

    Added lab06

---
+++ lab06/lab06/IService1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace lab06
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Add(int a, int b);
+    }
+}
---
+++ lab06/wcf-client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace wcf_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(lab06.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Service is running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
